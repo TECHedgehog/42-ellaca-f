c74e606384fb6df964ef3302361af23a7bddaf52
Eric Llaca fernandez <ellaca-f@student.42madrid.com>
2019-10-10 04:22:00 +0200

    third commit

--- CPiscineC02/ex00/fct0.c
+++ CPiscineC02/ex00/fct0.c
@@ -6,7 +6,7 @@
 /*   By: ellaca-f <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/09 20:08:23 by ellaca-f          #+#    #+#             */
-/*   Updated: 2019/10/09 20:25:24 by ellaca-f         ###   ########.fr       */
+/*   Updated: 2019/10/10 04:20:07 by ellaca-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,11 @@ char	*ft_strcpy(char *dest, char *src);
 
 int main()
 {
+	char *dest;
+	char *src;
 
+	src = "Hola como estamos?";
+	write(1, &*(ft_strcpy(dest, src)), 50);
+
+	return (0);
 }
