63714de2a38a64be3457f5052b8dd421b1462ea7
Eric Llaca fernandez <ellaca-f@student.42madrid.com>
2019-11-25 16:56:59 +0100

    twelfth 42 libtf/struct

--- 42Cursus/Libft/ft_strnstr.c
+++ 42Cursus/Libft/ft_strnstr.c
@@ -6,30 +6,30 @@
 /*   By: ellaca-f <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/05 13:24:29 by ellaca-f          #+#    #+#             */
-/*   Updated: 2019/11/21 20:39:53 by ellaca-f         ###   ########.fr       */
+/*   Updated: 2019/11/25 16:54:38 by ellaca-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+char	*ft_strnstr(const char *hstack, const char *ndle, size_t len)
 {
 	unsigned long int i;
 	unsigned long int j;
 
 	i = 0;
 	j = 0;
-	if (needle[0] == '\0')
-		return ((char*)haystack);
-	while (haystack[i] != '\0' && i < len)
+	if (ndle[0] == '\0')
+		return ((char*)hstack);
+	while (hstack[i] != '\0' && i < len)
 	{
 		j = 0;
-		if (haystack[i] == needle[j])
+		if (hstack[i] == ndle[j])
 		{
-			while (haystack[i + j] == needle[j] && needle[j] != '\0')
+			while (hstack[i + j] == ndle[j] && ndle[j] != '\0')
 				j++;
-			if (needle[j] == '\0' && i + j <= len)
-				return ((char*)&haystack[i]);
+			if (ndle[j] == '\0' && i + j <= len)
+				return ((char*)&hstack[i]);
 		}
 		i++;
 	}
