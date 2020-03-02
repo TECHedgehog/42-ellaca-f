a36a15243e2b983c12a565728961feb007cdea5d
Eric Llaca fernandez <ellaca-f@student.42madrid.com>
2020-03-02 12:00:14 +0100

    mac update

---
+++ 42Cursus/ft_printf/OTROS/tests/test.c
@@ -0,0 +1,24 @@
+#include "../ft_printf.h"
+int main()
+{
+	char *f = "hola1234";
+	int i = 0;
+	
+	write(1, &f[i++], 1);
+	
+	i = 0;
+	while (f[i])
+	{
+		write(1, &f[i], 1);
+		i++;
+	}
+	printf("\n");
+	int j = printf("12345");
+	printf("%d\n", j);
+	printf("%d\n", printf("12345"));
+	printf("\najfkd%04%\n");
+	printf("%.*s\n", 3, "abdefg");
+	printf("%0555d\n", 19);
+
+	return (0);
+}
